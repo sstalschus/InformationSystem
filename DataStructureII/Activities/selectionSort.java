ff205d2da9fcc89c2e87c23788418da2d1bb4b1d
Samuel Stalschus <70023098+SamStalschus@users.noreply.github.com>
2021-03-04 21:39:01 -0300

    Update selectionSort.java

--- DataStructureII/Activities/selectionSort.java
+++ DataStructureII/Activities/selectionSort.java
@@ -3,8 +3,6 @@ class selectionSort {
     
     int numbers[] = {7, 5, 4, 8, 2, 3, 0};
     int smaller, indexSmaller;
-
-    // procurando sempre o índice menor e atribuindo as primeiras posições
     for (int i = 0; i < numbers.length - 1; i++) {
         smaller = numbers[i];
         indexSmaller = i;
@@ -14,12 +12,9 @@ class selectionSort {
                 indexSmaller = j;
             }
         }
-        // array na posição do menor recebe o primeiro
-        // em seguida colocamos o menor em primeiro
         numbers[indexSmaller] = numbers[i];
         numbers[i] = smaller;
     }
-
     // print the array
     for(int j = 0; j < numbers.length ; j++){
        System.out.println(numbers[j]);
